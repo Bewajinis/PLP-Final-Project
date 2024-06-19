bff15299cf569bd10c4dba8e2b7d027aba0a2657
Emmanuel Bewaji <101416210+Bewajinis@users.noreply.github.com>
2024-06-19 20:47:03 +0100

    This is my python final project

--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,12 @@
 import re
+from django.http import JsonResponse
 from django.shortcuts import redirect, render
 from django.views import View
 from . models import Cart, Customer, Product
 from . forms import CustomerRegistrationForm, CustomerProfileForm
 from django.contrib import messages
 from django.db.models import Count
+from django.db.models import Q 
 
 # Create your views here.
 def home(request):
@@ -112,3 +114,23 @@ def show_cart(request):
         amount = amount + value
     totalamount = amount + 40
     return render(request, 'myapp/addtocart.html', locals())
+
+def plus_cart(request):
+    if request.method == 'GET':
+        prod_id=request.GET['prod_id']
+        c = Cart.objects.get(Q(Product-prod_id) & Q(user=request.user))
+        c.quantity +=1
+        c.save()
+        user = request.user
+        cart = Cart.objects.filter(user=user)
+        amount = 0
+        for p in cart:
+            value = p.quantity = p.product.discounted_price
+            amount = amount + value
+        totalamount = amount + 40
+        data={
+            'quantity': c.quantity,
+            'amount': amount,
+            'totalamount': totalamount
+        }
+        return JsonResponse(data)
